5f21f63fd100ba0c2b986d78548ce17ce9a6e6ba
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-07-06 11:02:16 +0300

    adding header label component;

---
+++ cox-report-ext-web/src/components/HeaderLabel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-subtitle-1 font-weight-bold">
+        {{ title }}: <span class="text-decoration-underline">{{ value }}</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderLabel',
+    props: {
+        value: {
+            type: [String, Number, Boolean],
+            default: null,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
