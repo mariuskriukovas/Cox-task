5a40d968afca8f999e79d5c38326be8d2d93ce7b
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-07-06 12:21:47 +0300

    implementing AttributeTable component;

---
+++ cox-report-ext-web/src/components/AttributeTable.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-data-table
+            :headers="headers"
+            :items="value"
+            :server-items-length="totalElements"
+            class="elevation-1"
+            hide-default-footer
+            item-key="uid"
+    >
+        <template v-slot:[`item.vin`]="{ item }">
+            <p>{{ item }}</p>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+
+export default {
+    name: 'AttributeTable',
+    props: {
+        value: {
+            type: Array,
+            default() {
+                return []
+            },
+            required: true,
+        },
+    },
+    computed: {
+        totalElements() {
+            return this.value?.length
+        }
+    },
+    data() {
+        return {
+            headers: [
+                {
+                    text: 'Attribute',
+                    align: 'start',
+                    value: 'attribute',
+                },
+                {
+                    text: 'Value',
+                    align: 'start',
+                    value: 'value',
+                },
+            ],
+        }
+    }
+}
+</script>
